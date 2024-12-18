3eae82abf7a5d81922fadc61e54c36314a6057a0
Ryan <126Rliu@frhsd.com>
2024-12-18 12:23:24 -0500

    first commit

---
+++ js/java.js
@@ -0,0 +1,8 @@
+let select = document.getElementById("states");
+select.addEventListener('change', function(){
+    let name = select.options[select.selectedIndex].text;
+    document.getElementById("postcard").style.backgroundImage = 'url("../img/'+this.value+'@2x.jpg")'
+    document.getElementById("greeting").innerText = 'Greetings from'
+    document.getElementById("state-heading").innerText = name
+}, false);
+
