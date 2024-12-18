65b36d53bcfa0938541bc82f2a837fb78ceca613
Ryan <126Rliu@frhsd.com>
2024-12-18 12:44:38 -0500

    stuff

--- js/java.js
+++ js/java.js
@@ -1,9 +1,9 @@
 let select = document.getElementById("states");
 select.addEventListener('change', function(){
     let name = select.options[select.selectedIndex].text;
-    document.getElementById("postcard").style.backgroundImage = 'url("../img/'+this.value+'@2x.jpg")';
+    document.getElementById("postcard").style.backgroundImage = 'url("/img/'+this.value+'@2x.jpg")';
     document.getElementById("greeting").innerText = 'Greetings from';
     document.getElementById("state-heading").innerText = name;
-    console.log('url("../img/'+this.value+'@2x.jpg")')
+    
 }, false);
 
